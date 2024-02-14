2292c055dcb1df9947fffbdc828288bfdf67cbcc
jorge <jrfaggiani@gmail.com>
2024-02-14 13:06:58 -0800

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * This is the preferred style for multi-line
- * comments in C source code.
- * Please use it consistently.
+ * main - Entry point
  *
- * Description:  A column of asterisks on the left side,
- * with beginning and ending almost-blank lines.
+ * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
