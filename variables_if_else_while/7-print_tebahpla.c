cfb57ae52e6118dfdd563524982cd4b6a7962c8a
jorge <jrfaggiani@gmail.com>
2024-02-16 05:02:23 -0800

    alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	return (0);
+}
