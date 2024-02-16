e273da0456e7a97d17de1e2f9ea2634acd91c682
jorge <jrfaggiani@gmail.com>
2024-02-16 03:59:18 -0800

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	return (0);
+}
