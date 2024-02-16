02f7d27f7305b8932366f5839373b483ea558c68
jorge <jrfaggiani@gmail.com>
2024-02-16 06:24:29 -0800

    number with commas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		putchar(number);
+		if (number != 57)
+		{
+			putchar(44);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
